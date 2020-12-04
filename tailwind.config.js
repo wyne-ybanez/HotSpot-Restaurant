0665cf905039f3c818e34f332c24a6cedf4471f5
wyne-ybanez <wyne_01@icloud.com>
2020-12-04 11:17:07 +0000

    Add cards and badges. Add css classes for cards. Add overall styling over the page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,10 @@ module.exports = {
           100: '#E2E2D5',
           200: '#888883',          
         }
-      }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      },
     },
   },
   variants: {
